9e8e19f1586c692e5d67995cad748eae91a66ab7
faizah1992 <faizah1992@gmail.com>
2020-10-13 16:23:30 -0500

    logger added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Raven from 'raven-js'
+import logService from './services/logService'
 
-Raven.config('https://05323d37c9a847eba9daaaab1e6171a9@sentry.io/1249956', {
-    release: '1-0-0',
-    environment: 'development-test',
-}).install();
+logger.init()
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
